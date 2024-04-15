700c86493fd2f6267110dfe16219ede6917675dc
sy070 <sy07038@gmail.com>
2024-04-16 01:30:27 +0900

    0401CSI-testcode

--- app/src/main/java/com/example/csi/Fragment5.kt
+++ app/src/main/java/com/example/csi/Fragment5.kt
@@ -5,18 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class Fragment5 : Fragment(), View.OnClickListener {
     private lateinit var rootView: View
-    private lateinit var button1: Button
-    private lateinit var button2: Button
-    private lateinit var button3: Button
-    private lateinit var button4: Button
-    private lateinit var button5: Button
-    private lateinit var button6: Button
-    private lateinit var button7: Button
+    private lateinit var imageButton1: ImageView
+    private lateinit var imageButton2: ImageView
+    private lateinit var imageButton3: ImageView
+    private lateinit var imageButton4: ImageView
+    private lateinit var imageButton5: ImageView
+    private lateinit var imageButton6: ImageView
+    private lateinit var imageButton7: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,52 +25,52 @@ class Fragment5 : Fragment(), View.OnClickListener {
     ): View {
         rootView = inflater.inflate(R.layout.fragment5, container, false)
 
-        // 버튼을 XML에서 찾아 변수에 할당
-        button1 = rootView.findViewById(R.id.button1)
-        button2 = rootView.findViewById(R.id.button2)
-        button3 = rootView.findViewById(R.id.button3)
-        button4 = rootView.findViewById(R.id.button4)
-        button5 = rootView.findViewById(R.id.button5)
-        button6 = rootView.findViewById(R.id.button6)
-        button7 = rootView.findViewById(R.id.button7)
+        // 이미지 뷰를 XML에서 찾아 변수에 할당
+        imageButton1 = rootView.findViewById(R.id.imagebutton1)
+        imageButton2 = rootView.findViewById(R.id.imagebutton2)
+        imageButton3 = rootView.findViewById(R.id.imagebutton3)
+        imageButton4 = rootView.findViewById(R.id.imagebutton4)
+        imageButton5 = rootView.findViewById(R.id.imagebutton5)
+        imageButton6 = rootView.findViewById(R.id.imagebutton6)
+        imageButton7 = rootView.findViewById(R.id.imagebutton7)
 
-        // 각 버튼에 클릭 리스너 설정
-        button1.setOnClickListener(this)
-        button2.setOnClickListener(this)
-        button3.setOnClickListener(this)
-        button4.setOnClickListener(this)
-        button5.setOnClickListener(this)
-        button6.setOnClickListener(this)
-        button7.setOnClickListener(this)
+        // 각 이미지 뷰에 클릭 리스너 설정
+        imageButton1.setOnClickListener(this)
+        imageButton2.setOnClickListener(this)
+        imageButton3.setOnClickListener(this)
+        imageButton4.setOnClickListener(this)
+        imageButton5.setOnClickListener(this)
+        imageButton6.setOnClickListener(this)
+        imageButton7.setOnClickListener(this)
 
         return rootView
     }
 
     override fun onClick(v: View?) {
-        // 클릭된 버튼에 따라 해당하는 액티비티를 시작
+        // 클릭된 이미지 뷰에 따라 해당하는 액티비티를 시작
         when (v?.id) {
-            R.id.button1 -> {
+            R.id.imagebutton1 -> {
                 startActivity(Intent(activity, SearchActivity::class.java))
             }
-            R.id.button2 -> {
+            R.id.imagebutton2 -> {
                 startActivity(Intent(activity, SearchActivity1::class.java))
             }
-            R.id.button3 -> {
+            R.id.imagebutton3 -> {
                 startActivity(Intent(activity, SearchActivity2::class.java))
             }
-            R.id.button4 -> {
+            R.id.imagebutton4 -> {
                 startActivity(Intent(activity, SearchActivity3::class.java))
             }
-            R.id.button5 -> {
+            R.id.imagebutton5 -> {
                 startActivity(Intent(activity, SearchActivity4::class.java))
             }
-            R.id.button6 -> {
+            R.id.imagebutton6 -> {
                 startActivity(Intent(activity, SearchActivity5::class.java))
             }
-            R.id.button7 -> {
+            R.id.imagebutton7 -> {
                 startActivity(Intent(activity, SearchActivity6::class.java))
             }
-            // 나머지 버튼들에 대한 처리 추가
+            // 나머지 이미지 뷰들에 대한 처리 추가
         }
     }
 }

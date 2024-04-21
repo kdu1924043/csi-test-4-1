5e1322580c1a9cb497ace8e4d012f2c18a39aea5
sy070 <sy07038@gmail.com>
2024-04-22 01:19:04 +0900

    0401CSI-testcode

--- app/src/main/java/com/example/csi/Fragment5.kt
+++ app/src/main/java/com/example/csi/Fragment5.kt
@@ -24,6 +24,14 @@ class Fragment5 : Fragment(), View.OnClickListener {
     private lateinit var imageButton12: ImageView
     private lateinit var imageButton13: ImageView
     private lateinit var imageButton14: ImageView
+    private lateinit var imageButton15: ImageView
+    private lateinit var imageButton16: ImageView
+    private lateinit var imageButton17: ImageView
+    private lateinit var imageButton18: ImageView
+    private lateinit var imageButton19: ImageView
+    private lateinit var imageButton20: ImageView
+    private lateinit var imageButton21: ImageView
+    private lateinit var imageButton22: ImageView
 
 
     override fun onCreateView(
@@ -48,6 +56,15 @@ class Fragment5 : Fragment(), View.OnClickListener {
         imageButton12 = rootView.findViewById(R.id.imagebutton12)
         imageButton13 = rootView.findViewById(R.id.imagebutton13)
         imageButton14 = rootView.findViewById(R.id.imagebutton14)
+        imageButton15 = rootView.findViewById(R.id.imagebutton15)
+        imageButton16 = rootView.findViewById(R.id.imagebutton16)
+        imageButton17 = rootView.findViewById(R.id.imagebutton17)
+        imageButton18 = rootView.findViewById(R.id.imagebutton18)
+        imageButton19 = rootView.findViewById(R.id.imagebutton19)
+        imageButton20 = rootView.findViewById(R.id.imagebutton20)
+        imageButton21 = rootView.findViewById(R.id.imagebutton21)
+        imageButton22 = rootView.findViewById(R.id.imagebutton22)
+
 
         // 각 이미지 뷰에 클릭 리스너 설정
         imageButton1.setOnClickListener(this)
@@ -64,6 +81,16 @@ class Fragment5 : Fragment(), View.OnClickListener {
         imageButton12.setOnClickListener(this)
         imageButton13.setOnClickListener(this)
         imageButton14.setOnClickListener(this)
+        imageButton15.setOnClickListener(this)
+        imageButton16.setOnClickListener(this)
+        imageButton17.setOnClickListener(this)
+        imageButton18.setOnClickListener(this)
+        imageButton19.setOnClickListener(this)
+        imageButton20.setOnClickListener(this)
+        imageButton21.setOnClickListener(this)
+        imageButton22.setOnClickListener(this)
+
+
 
 
         return rootView
@@ -114,6 +141,31 @@ class Fragment5 : Fragment(), View.OnClickListener {
             R.id.imagebutton14 -> {
                 startActivity(Intent(activity, SearchActivity13::class.java))
             }
+            R.id.imagebutton15 -> {
+                startActivity(Intent(activity, SearchActivity14::class.java))
+            }
+            R.id.imagebutton16 -> {
+                startActivity(Intent(activity, SearchActivity15::class.java))
+            }
+            R.id.imagebutton17 -> {
+                startActivity(Intent(activity, SearchActivity16::class.java))
+            }
+            R.id.imagebutton18 -> {
+                startActivity(Intent(activity, SearchActivity17::class.java))
+            }
+            R.id.imagebutton19 -> {
+                startActivity(Intent(activity, SearchActivity18::class.java))
+            }
+            R.id.imagebutton20 -> {
+                startActivity(Intent(activity, SearchActivity19::class.java))
+            }
+            R.id.imagebutton21 -> {
+                startActivity(Intent(activity, SearchActivity20::class.java))
+            }
+            R.id.imagebutton22 -> {
+                startActivity(Intent(activity, SearchActivity21::class.java))
+            }
+
             // 나머지 이미지 뷰들에 대한 처리 추가
         }
     }

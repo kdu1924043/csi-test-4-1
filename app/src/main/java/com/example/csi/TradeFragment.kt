503e017fe23e28324a51047efea41eeed4d26c0e
sy070 <sy07038@gmail.com>
2024-05-16 13:27:29 +0900

    0401CSI-testcode

--- app/src/main/java/com/example/csi/TradeFragment.kt
+++ app/src/main/java/com/example/csi/TradeFragment.kt
@@ -19,18 +19,5 @@ class TradeFragment : Fragment(R.layout.fragment7) {
             val intent = Intent(context, BuyActivity::class.java)
             startActivity(intent)
         }
-
-
-        val imageView1 = view.findViewById<ImageView>(R.id.imagebutton2)
-        // ImageView 사용 예: 클릭 리스너 설정
-        imageView1.setOnClickListener {
-            // 이미지 클릭 시 수행할 작업
-        }
-
-        val imageView2 = view.findViewById<ImageView>(R.id.imagebutton3)
-        // ImageView 사용 예: 클릭 리스너 설정
-        imageView2.setOnClickListener {
-            // 이미지 클릭 시 수행할 작업
-        }
     }
 }

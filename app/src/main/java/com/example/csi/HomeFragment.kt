6305166c276446a1db5028918f560271c451225e
sy070 <sy07038@gmail.com>
2024-05-21 03:10:52 +0900

    0401CSI-testcode

--- app/src/main/java/com/example/csi/HomeFragment.kt
+++ app/src/main/java/com/example/csi/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.csi
 
+
 import ImagePagerAdapter
+import ItemAdapter1
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -10,20 +12,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.csi.databinding.Fragment4Binding
+import org.json.JSONArray
 import java.util.Timer
 import java.util.TimerTask
+import org.json.JSONObject
+import java.io.IOException
+import java.nio.charset.Charset
 
 class HomeFragment : Fragment() {
     private var _binding: Fragment4Binding? = null
     private val binding get() = _binding!!
-
     private lateinit var viewPager: ViewPager
     private lateinit var timer: Timer
-
-
     private val delay: Long = 3000 // 슬라이드 간격 (3초)
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter1: ItemAdapter1
+    private lateinit var itemList: MutableList<Item1>
+    private lateinit var filteredList: MutableList<Item1>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,14 +55,35 @@ class HomeFragment : Fragment() {
             transaction.commit()
         }
 
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
         // ViewPager를 초기화하고 이미지 슬라이드 배너 설정
         viewPager = binding.viewPager
-        val images = arrayOf(R.drawable.a, R.drawable.b, R.drawable.c,R.drawable.d,R.drawable.f,R.drawable.g)
+        val images = arrayOf(R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.f, R.drawable.g)
         val adapter = ImagePagerAdapter(requireContext(), images, onImageClickListener)
         viewPager.adapter = adapter
         startAutoSlide()
-    }
 
+        val json: String = loadJSONFromAsset("hotis.json") ?: ""
+
+        val jsonArray = JSONArray(json)
+        itemList = mutableListOf()
+        for (i in 0 until jsonArray.length()) {
+            val jsonObject: JSONObject = jsonArray.getJSONObject(i)
+            val item1 = Item1(
+                jsonObject.getInt("no"),
+                jsonObject.getString("name"),
+                jsonObject.getInt("price"),
+                jsonObject.getString("photo")
+            )
+            itemList.add(item1)
+        }
+
+        filteredList = ArrayList(itemList)
+        adapter1 = ItemAdapter1(filteredList)
+        recyclerView.adapter = adapter1
+    }
     private fun startAutoSlide() {
         timer = Timer()
         timer.schedule(object : TimerTask() {
@@ -68,6 +98,23 @@ class HomeFragment : Fragment() {
             }
         }, delay, delay)
     }
+
+
+    private fun loadJSONFromAsset(fileName: String): String? {
+        var json: String? = null
+        try {
+            val inputStream = requireContext().assets.open(fileName)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            json = String(buffer, Charset.forName("UTF-8"))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return json
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()

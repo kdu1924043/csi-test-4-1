58ae13de36e76c93344b77b4306aa60b4cb11b86
sy070 <sy07038@gmail.com>
2024-05-25 22:36:53 +0900

    0401CSI-testcode

--- app/src/main/java/com/example/csi/TradeFragment.kt
+++ app/src/main/java/com/example/csi/TradeFragment.kt
@@ -13,10 +13,16 @@ class TradeFragment : Fragment(R.layout.fragment7) {
         super.onViewCreated(view, savedInstanceState)
 
         val imageView = view.findViewById<ImageView>(R.id.imagebutton1)
+        val imageView2 = view.findViewById<ImageView>(R.id.imagebutton2)
 // ImageView 클릭 리스너 설정
         imageView.setOnClickListener {
             // Intent 생성 및 BuyActivity로 이동
             val intent = Intent(context, BuyActivity::class.java)
+            startActivity(intent)
+        }
+        imageView2.setOnClickListener {
+            // Intent 생성 및 BuyActivity로 이동
+            val intent = Intent(context, Buy2Activity::class.java)
             startActivity(intent)
         }
     }

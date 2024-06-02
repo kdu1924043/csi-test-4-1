4f897a7d1f78b9df23ee1d45c96ab46f26b5b295
sy070 <sy07038@gmail.com>
2024-06-02 21:12:15 +0900

    0401CSI-testcode

--- app/src/main/java/com/example/csi/Review.kt
+++ app/src/main/java/com/example/csi/Review.kt
@@ -1,6 +1,7 @@
 package com.example.csi
 
 data class Review(
+    val userEmail: String? = "",
     val itemName: String? = null,
     val reviewText: String? = null
 )
--- app/src/main/java/com/example/csi/ReviewActivity.kt
+++ app/src/main/java/com/example/csi/ReviewActivity.kt
@@ -1,4 +1,5 @@
 package com.example.csi
+
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -7,6 +8,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
 class ReviewActivity : AppCompatActivity() {
@@ -18,6 +20,7 @@ class ReviewActivity : AppCompatActivity() {
     private lateinit var submitButton: Button
     private lateinit var reviewTextView: TextView
     private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,17 +43,21 @@ class ReviewActivity : AppCompatActivity() {
 
         // Firebase Realtime Database의 "reviews" 노드에 대한 참조 가져오기
         database = FirebaseDatabase.getInstance().reference.child("reviews")
+        auth = FirebaseAuth.getInstance() // FirebaseAuth 인스턴스 초기화
 
         submitButton.setOnClickListener {
             val reviewText = reviewEditText.text.toString()
-            if (item != null && reviewText.isNotEmpty()) {
+            val currentUser = auth.currentUser // 현재 로그인한 사용자 가져오기
+            if (item != null && reviewText.isNotEmpty() && currentUser != null) {
+                val userEmail = currentUser.email // 현재 사용자의 이메일 가져오기
                 val reviewData = mapOf(
                     "itemName" to item.name,
-                    "reviewText" to reviewText
+                    "reviewText" to reviewText,
+                    "userEmail" to userEmail // 이메일 주소 추가
                 )
-                val reviewId = database.child("reviews").push().key // 리뷰에 대한 고유한 키 생성
+                val reviewId = database.push().key // 리뷰에 대한 고유한 키 생성
                 if (reviewId != null) {
-                    database.child("reviews").child(reviewId).setValue(reviewData)
+                    database.child(reviewId).setValue(reviewData)
                         .addOnSuccessListener {
                             // 저장 성공 처리
                         }
@@ -63,36 +70,31 @@ class ReviewActivity : AppCompatActivity() {
             }
         }
 
-
-
         // Firebase Realtime Database에서 리뷰를 읽고 업데이트
-        // Firebase Realtime Database에서 리뷰를 읽고 업데이트
-        database.child("reviews").addValueEventListener(object : ValueEventListener {
+        database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 Log.d("ReviewActivity", "onDataChange called")
                 val reviews = mutableListOf<String>()
                 for (reviewSnapshot in snapshot.children) {
                     val itemName = reviewSnapshot.child("itemName").getValue(String::class.java)
                     val reviewText = reviewSnapshot.child("reviewText").getValue(String::class.java)
+                    val userEmail = reviewSnapshot.child("userEmail").getValue(String::class.java)
                     if (itemName != null && reviewText != null && itemName == item?.name) { // 현재 보고 있는 상품에 해당하는 리뷰만 추가
-                        val review = "$itemName: $reviewText"
+                        val review = "$userEmail:$reviewText"
                         reviews.add(review)
                     }
                 }
                 updateReviews(reviews)
             }
+
             override fun onCancelled(error: DatabaseError) {
                 // 오류 처리
             }
         })
-
-
-
     }
 
     private fun updateReviews(reviews: List<String>) {
         val reviewsText = reviews.joinToString("\n")
         reviewTextView.text = reviewsText
     }
-
 }
